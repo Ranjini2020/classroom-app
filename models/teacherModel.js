cc606947d797378e27b3aa83c4db89facaa3a377
unknown <avanidhalgara01@gmail.com>
2020-08-17 16:57:02 -0500

    login created and login page

---
+++ models/teacherModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 5 },
+  displayName: { type: String },
+  classArray:[]
+});
+
+
+module.exports = Teacher = mongoose.model("teacher", userSchema);
