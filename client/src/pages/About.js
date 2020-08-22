5d66f2a5421c1390917c6ede0bb5d26463e8780c
Sriranjini Baskaran <ranjini_baskaran@yahoo.com>
2020-08-22 00:24:52 -0500

    Home page information

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,20 +1,53 @@
 import React from "react";
+import Home from '../components/home';
+import Container from "../components/container";
+import Row from "../components/row";
+import Col from "../components/col";
 import Header from '../components/Header';
 
 function About() {
   return (
     <div>
       <Header/>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <Home backgroundImage="https://www.cambridge.org/elt/blog/wp-content/uploads/2014/03/iStock_000016678177Medium1.jpg">
+        <h1>HUGO</h1>
+        <h2>Helps you move forward!!</h2>
+      </Home>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Welcome To HUGO Classroom!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
+              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
+              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
+              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
+              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
+              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
+              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+            <p>
+              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
+              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
+              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
+              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
+              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
+              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+            </p>
+            
+          </Col>
+       </Row>
+       </Container>
     </div>
-  );
-}
+//   );
+  )};
 
 export default About;
